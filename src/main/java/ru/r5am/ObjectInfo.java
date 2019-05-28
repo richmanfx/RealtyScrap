b7cd60451b1631988ddc9b542850d65cf0d5816f
Александр Ящук <a.yashuk@pflb.ru>
2019-05-28 07:31:30 +0300

    Выравнивание в ячейках HTML таблицы, CSS-файл, разбиты заголовки таблицы для лучшего переноса.

--- src/main/java/ru/r5am/ObjectInfo.java
+++ src/main/java/ru/r5am/ObjectInfo.java
@@ -10,7 +10,7 @@ class ObjectInfo {
     @Getter @Setter     String  address;                    // Адрес
     @Getter @Setter     String  area;                       // Площадь, кв.м
     @Getter @Setter     String  monthlyRental;              // Стоимость аренды в месяц, рублей
-    @Getter @Setter     String  rentalPeriod;               // Срок аренды, месяцев
+    @Getter @Setter     String  rentalPeriod;               // Срок аренды
     @Getter @Setter     String  webLink;                    // Ссылка для просмотра
     @Getter @Setter     String  auctionData;                // Дата проведения аукциона
     @Getter @Setter     String  closingApplicationsDate;    // Дата окончания подачи заявок
